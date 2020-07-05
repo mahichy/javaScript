954219e96469f3a622c4a791dcd7810b519d9822
mahi <mohiddinoshim@yahoo.com>
2020-07-06 02:13:28 +0600

    C_49 Event Handling

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -9,7 +9,7 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 
 
@@ -17,17 +17,62 @@ scores = [0,0];
 roundScore = 0;
 activePlayer = 1;
 
-dice = Math.floor(Math.random() *6) + 1;
-console.log(dice);
-
-document.querySelector('#current-' + activePlayer).textContent = dice; 
-// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+// dice = Math.floor(Math.random() *6) + 1;
 
 
-var x = document.querySelector('#score-0').textContent;
-console.log(x);
 
 document.querySelector('.dice').style.display = 'none';
+
+
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+	
+	// 1. Rendom number
+	var dice = Math.floor(Math.random() *6) + 1;
+
+	// Display the result
+	var diceDOM = document.querySelector('.dice');
+	 diceDOM.style.display = 'block';
+	 diceDOM.src = 'dice-' + dice + '.png';
+
+
+
+	// Update the round score IF the rolled number was NOT a 1
+
+
+});
+
+
+
+// document.querySelector('#current-' + activePlayer).textContent = dice; 
+// // document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+// var x = document.querySelector('#score-0').textContent;
+// console.log(x);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
