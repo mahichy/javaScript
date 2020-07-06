c39d8b94e8d1c220739a2c4c975418fef275a555
mahi <mohiddinoshim@yahoo.com>
2020-07-06 21:28:25 +0600

    C_50 Updating Scores and Changing the Active Player

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -15,7 +15,7 @@ var scores, roundScore, activePlayer;
 
 scores = [0,0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
 // dice = Math.floor(Math.random() *6) + 1;
 
@@ -45,13 +45,38 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 
 
 	// Update the round score IF the rolled number was NOT a 1
+	if (dice !== 1) {
+			//Add score 
+			roundScore += dice;
+			document.querySelector('#current-' + activePlayer).textContent = roundScore;
+	}else {
+		// Next Player
+		activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+		roundScore = 0;
+
+		document.getElementById('current-0').textContent = '0';  
+		document.getElementById('current-1').textContent = '0'; 
+
+
+		document.querySelector('.player-0-panel').classList.toggle('active');
+		document.querySelector('.player-1-panel').classList.taggle('active');
+
+
+
+		// document.querySelector('.player-0-panel').classList.remove('active');
+		// document.querySelector('.player-1-panel').classList.add('active');
+
+		// document.querySelector('.dice').style.display = 'none';
+
+	}
+
 
 
 });
 
 
 
-// document.querySelector('#current-' + activePlayer).textContent = dice; 
+// = dice; 
 // // document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
 // var x = document.querySelector('#score-0').textContent;
 // console.log(x);
