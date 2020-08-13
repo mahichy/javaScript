c7626a6280cc6b2063d6ccf8351d6fa707fa07aa
mahi <mohiddinoshim@yahoo.com>
2020-08-13 16:13:01 +0600

    C_54 coading challange 6

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -58,7 +58,7 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 
 			// check if player won the game
 
-			if (scores[activePlayer] >=20) {
+			if (scores[activePlayer] >=100) {
 				document.querySelector('#name-' + activePlayer).textContent = 'winner!';
 				document.querySelector('.dice').style.display = 'none';
 				document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
@@ -123,6 +123,16 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 // // document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
 // var x = document.querySelector('#score-0').textContent;
 // console.log(x);
+
+
+/*
+YOUR 3 CHALLENGES
+Change the game to follow these rules:
+
+1. A player looses his ENTIRE score when he rolls two 6 in a row. After that, it's the next player's turn. (Hint: Always save the previous dice roll in a separate variable)
+2. Add an input field to the HTML where players can set the winning score, so that they can change the predefined score of 100. (Hint: you can read that value with the .value property in JavaScript. This is a good oportunity to use google to figure this out :)
+3. Add another dice to the game, so that there are two dices now. The player looses his current score when one of them is a 1. (Hint: you will need CSS to position the second dice, so take a look at the CSS code for the first one.)
+*/
 
 
 
